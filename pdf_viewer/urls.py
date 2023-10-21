ef01ce00522fe4ca87ed64a8ed028c03f3436230
WandersoNoleto <onlywanderson@gmail.com>
2023-10-21 10:40:14 -0300

    (#002) pdf viewer: pdf.js test02

--- pdf_viewer/urls.py
+++ pdf_viewer/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from pdf_viewer import views
 
 urlpatterns = [
-    path('pdf-viewer/', views.home, name="Home"),
+    path('', views.home, name="Home"),
     path('save-pdf/', views.save_pdf, name="save-pdf")
 ]
