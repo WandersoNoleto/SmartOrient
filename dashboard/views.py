17ef59a01c41450fd6bb7e383105f771c0fc00d0
WandersoNoleto <onlywanderson@gmail.com>
2023-11-15 14:08:51 -0300

    add guinance list screen

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from guidances.models import Guidance
+from users.models import Student
+
 
 @login_required(login_url= '/auth/login/')
 def home(request):
-    return render(request, 'home.html')
+    guidances      = Guidance.objects.all()
+    logged_user_id = request.user.id
+    logged_user    = Student.objects.filter(genericuser_ptr_id=logged_user_id).first()
+
+    context = {
+        'guidances': guidances,
+        'logged_user': logged_user
+        }
+
+    return render(request, 'base.html', context)
--- guidances/models.py
+++ guidances/models.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 from string import ascii_uppercase, digits
 
 from django.db import models
@@ -7,23 +8,23 @@ from users.models import Advisor, Coordination, Student
 
 
 class Guidance(models.Model):
-    PROJECT_CHOICES = [
-        ('Extensao', 'Projeto de Extensão'),
-        ('Pesquisa', 'Projeto de Pesuisa'),
-        ('TCC', 'Trabalho de Conclusão de Curso'),
-        ('Outro', 'Outro')
-    ]
 
-    project_type  = models.CharField(max_length=50, choices=PROJECT_CHOICES, verbose_name="Tipo de projeto")
     project_title = models.CharField(max_length=100, verbose_name="Título do projeto")
     student       = models.ForeignKey(Student, on_delete=models.CASCADE, max_length=100, blank=False, null=False, verbose_name="Estudante")
     advisor       = models.ForeignKey(Advisor, on_delete=models.CASCADE, max_length=100, blank=False, null=False, verbose_name="Orientador")
     coordination  = models.ForeignKey(Coordination, on_delete=models.SET_NULL, max_length=100, blank=True, null=True, verbose_name="Coordenação")
+    status        = models.CharField(max_length=50, default="Em andamento", verbose_name="Status")
+    start_date    = models.DateField(verbose_name="Data de início")
     guidance_code = models.CharField(max_length=6, unique=True, verbose_name="Código Da Orientação")
+
 
     def generate_guidance_code(self):
         return ''.join(random.choice(ascii_uppercase + digits) for _ in range(6))
-        
+    
+    def set_start_date(self):
+        date = datetime.now().strftime("%d/%m/%Y")
+        self.start_date = date
+
 
     def save(self, *args, **kwargs):
         if not self.guidance_code:
