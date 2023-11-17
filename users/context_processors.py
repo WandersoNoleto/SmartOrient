552c0f3a53aa68825117f9b9cbdc4b69540c68ed
WandersoNoleto <onlywanderson@gmail.com>
2023-11-17 15:44:05 -0300

    (#006)auth: context_processors for logged_user

--- users/context_processors.py
+++ users/context_processors.py
@@ -1,7 +1,21 @@
-from .models import Student
+from .models import Advisor, Coordination, Student
 
 
 def logged_user(request):
     logged_user_id = request.user.id
-    user = Student.objects.filter(genericuser_ptr_id=logged_user_id).first() if logged_user_id else None
-    return {'logged_user': user, 'full_name': user.abbreviate_name if user else ''}
+    student = Student.objects.filter(genericuser_ptr_id=logged_user_id).first()
+    advisor = Advisor.objects.filter(genericuser_ptr_id=logged_user_id).first()
+    coordination = Coordination.objects.filter(genericuser_ptr_id=logged_user_id).first()
+
+    if student:
+        user = student
+        identifier_field = user.abbreviate_name if user else ''
+    elif advisor:
+        user = advisor
+        identifier_field = user.abbreviate_name if user else ''
+    elif coordination:
+        user = coordination
+        identifier_field = user.code_format if user else ''
+
+
+    return {'logged_user': user, 'user_identifier_field': identifier_field}
--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+import string
+
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
@@ -45,6 +47,17 @@ class Advisor(GenericUser):
     phone_number = models.CharField(max_length=50, verbose_name="Número de Telefone")
     institution = models.CharField(max_length=50, verbose_name="Instituição")
 
+    def abbreviate_name(self):
+        parts = self.full_name.split()
+
+        if len(parts) < 3:
+            return self.full_name
+
+        abbreviated = [parts[0]] + [f"{name[0]}." if name.lower() not in {"e", "de"} else name for name in parts[1:-1]] + [parts[-1]]
+        abbreviated_name = ' '.join(abbreviated)
+
+        return abbreviated_name
+
     def __str__(self):
         return self.full_name
     
@@ -54,6 +67,12 @@ class Coordination(GenericUser):
     institution = models.CharField(max_length=50, verbose_name="Instituição")
     campus      = models.CharField(max_length=50, verbose_name="Campus")
     code        = models.CharField(unique=True, help_text="Unique per coordination: institution.campus-course", max_length=50, verbose_name="Código: institution.campus-course")
+
+
+    def code_format(self):
+        formated_code = string.upper(self.code)
+
+        return formated_code
 
     def __str__(self):
         return self.code
