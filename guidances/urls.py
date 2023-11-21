455fb78ad780924213857fed2b753f45d5aa58e3
WandersoNoleto <onlywanderson@gmail.com>
2023-11-21 15:52:14 -0300

    (#002) accept_pending_guidances working

--- guidances/urls.py
+++ guidances/urls.py
@@ -5,5 +5,7 @@ from guidances import views
 urlpatterns = [
     path('register-guidance-page/student', views.register_guidance_page, name="register_guidance_page"),
     path('register-guidance-save/', views.register_guidance_save, name="register_guidance_save"),
-    path('pending-guidances/', views.guidances_pending_page, name="guidances_pending_page")
+    path('pending-guidances/', views.guidances_pending_page, name="guidances_pending_page"),
+    path('accept-guidance/<int:id>/', views.pending_guidance_accept, name="pending_guidance_accept"),
+    path('delete-guidance/<int:id>/', views.delete_guidance, name="delete_guidance")
 ]
--- guidances/views.py
+++ guidances/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from guidances.models import Guidance
 from users.models import Advisor, Coordination, Student
@@ -47,7 +47,21 @@ def guidances_pending_page(request):
     pending_guidances = Guidance.objects.filter(status="Pendente", advisor_id=request.user.id)
 
     context = {
-        'pending_guidances': pending_guidances
+        'pending_guidances': pending_guidances,
     }
 
     return render(request, 'acceptGuidance.html', context)
+
+def pending_guidance_accept(request, id):
+    guidance = get_object_or_404(Guidance, id=id)
+    if guidance.status == "Pendente":
+        guidance.status = "Em andamento"
+        guidance.save()
+
+    return redirect("home_advisor")
+
+def delete_guidance(request, id):
+    guidance = Guidance.objects.filter(id=id)
+    guidance.delete()
+
+    return redirect("home_advisor")
