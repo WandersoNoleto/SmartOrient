e98da01658ea66b9710fd95776dbfc239184fa48
WandersoNoleto <onlywanderson@gmail.com>
2023-11-24 22:59:42 -0300

    integration with postgres

--- smartorient/settings.py
+++ smartorient/settings.py
@@ -74,8 +74,11 @@ WSGI_APPLICATION = 'smartorient.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SmartOrient',
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': 'localhost'
     }
 }
 
